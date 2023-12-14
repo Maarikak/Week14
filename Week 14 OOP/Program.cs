2a318ec12e6ac9f6771acfeb01e1cb3fcd26e095
MaarikaK <testmaarika@gmail.com>
2023-12-15 00:21:39 +0200

    Week14

---
+++ Week 14 OOP/Program.cs
@@ -0,0 +1,45 @@
+﻿
+using System.Net.Security;
+
+Dog goodboy = new Dog("Good Boy");
+Console.WriteLine($"Name: {goodboy.Name}; Level of happinness : {goodboy.LevelOfHappinness}");
+
+while(goodboy.LevelOfHappinness < 5)
+{
+    goodboy.Bark();
+}
+
+goodboy.Wag();
+
+//goodboy.Bark();
+//Console.WriteLine($"Name: {goodboy.Name}; Level of happinness : {goodboy.LevelOfHappinness}");
+//goodboy.Wag();
+
+
+class Dog
+{
+    public string Name;
+    public int LevelOfHappinness;
+
+    public Dog(string name)
+    {
+        Name = name;
+        LevelOfHappinness = 0;
+        Console.WriteLine($"{Name} is your friend now.");
+    }
+
+    public void Bark()
+    {
+        LevelOfHappinness++;
+        Console.WriteLine("Woof-woof!");
+    }
+
+    public void Wag()
+    {
+        Console.WriteLine($"{Name} starts wagging its tail.");
+        Console.WriteLine("Whoosh-woosh");
+    }
+
+
+
+}
